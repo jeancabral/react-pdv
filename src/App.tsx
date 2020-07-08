1d6695fb3e8ae11e7ca0f7d5c564ed66e2633177
Jean Cabral <jeancabral@gmail.com>
2020-07-08 19:16:28 -0300

    add routes and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 const App: React.FC = () => (
   <>
-    <h1>ReactJS Starter com TS by Jeazinho</h1>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
   </>
 );
 
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Dashboard: React.FC = () => (
+  <h1>Dashboard</h1>
+);
+
+export default Dashboard;
