87bc84e9f670081b631f62be60e2d2ff3394cb33
Jean Cabral <jeancabral@gmail.com>
2020-07-07 21:18:23 -0300

    config ESlint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <>
-        <h1>ReactJS Starter com TS by Jeazinho</h1>
-    </>
-  );
-}
+const App: React.FC = () => (
+  <>
+    <h1>ReactJS Starter com TS by Jeazinho</h1>
+  </>
+);
 
 export default App;
