9b725b805a7db3a9c8c5b0de7956e178fe5539fb
Jean Cabral <jeancabral@gmail.com>
2020-07-11 18:27:11 -0300

    style: carrousel category products

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,6 +3,11 @@ import {
   FiDollarSign, FiCreditCard, FiHardDrive, FiSearch,
 } from 'react-icons/fi';
 
+import {
+  CarouselProvider, Slider, Slide, ButtonBack, ButtonNext,
+} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
 import * as s from './styled';
 import Layout from '../../components/Layout';
 
@@ -47,6 +52,19 @@ const Dashboard: React.FC = () => (
       <FiSearch size="24px" color="grey" />
       <s.SearchInput />
     </s.Search>
+    <CarouselProvider
+      naturalSlideWidth={100}
+      naturalSlideHeight={125}
+      totalSlides={3}
+    >
+      <Slider>
+        <Slide index={0}>I am the first Slide.</Slide>
+        <Slide index={1}>I am the second Slide.</Slide>
+        <Slide index={2}>I am the third Slide.</Slide>
+      </Slider>
+      <ButtonBack>Back</ButtonBack>
+      <ButtonNext>Next</ButtonNext>
+    </CarouselProvider>
   </Layout>
 );
 
