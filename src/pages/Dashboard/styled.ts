82e19fc69b74e9d743e3395f3ed5d879cdc00d11
Jean Cabral <jeancabral@gmail.com>
2020-07-17 20:41:08 -0300

    style: fix carrousel

--- src/pages/Dashboard/styled.ts
+++ src/pages/Dashboard/styled.ts
@@ -135,14 +135,14 @@ export const CategoryContainer = styled.div`
 
 export const CategoryItem = styled.div`
 
-  background: #ffffff;
+background: #ffffff;
   box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.1);
   border-radius: 10px;
-  width: auto;
-  margin: 25px;
-  max-width: 100%;
-
-  padding: 35px;
+  width: 100%;
+  margin: 25px auto;
+  max-width: 116px;
+  height: 144px;
+  padding: 18px;
   border-radius: 5px;
   display: flex;
   flex-direction: column;
@@ -170,10 +170,46 @@ export const CategoryItem = styled.div`
     transform: scale(1.2);
   }
 
-  ${media.lessThan('large')`
-    margin: 35px 18px;
-    padding: 20px;
-    height: auto;
+
+
+  ${media.greaterThan('large')`
+  background: #ffffff;
+  box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  width: auto;
+  margin: 25px;
+  max-width: 100%;
+height: auto;
+  padding: 20px;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  transition: 0.3s;
+
+  header {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    flex-direction: column-reverse;
+    justify-content: space-between;
+    p {
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      margin-top: 10px;
+      line-height: 19px;
+      letter-spacing: -0.274286px;
+      color: #292c48;
+    }
+  }
+
+  &:hover {
+    box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.1);
+    transform: scale(1.2);
+  }
+
   `}
+
+
 
 `;
